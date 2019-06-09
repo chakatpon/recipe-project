e10c7c302293a19011ec7c0af778e41373370087
Vanoldschool\Van_oldschool <DoctorVan-za@hotmail.com>
2019-06-10 02:08:18 +0700

    add recipe.model.ts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [ 
+    new Recipe("Guitar", "I love music.", "https://thumbs.gfycat.com/SizzlingQuerulousIrishwaterspaniel-size_restricted.gif"),
+    new Recipe("Game", "I love to play game.", "https://media.giphy.com/media/HDaAnwtsSge0E/giphy.gif"),
+  ];
 
   constructor() { }
 
